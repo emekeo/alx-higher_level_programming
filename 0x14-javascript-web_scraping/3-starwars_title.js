2a766edc53951d2af032d0ddac3f6dcff58898a8
emekeo <gistwithcollins@gmail.com>
2023-02-09 03:47:01 -0800

    Web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const BASE_URL = 'https://swapi-api.alx-tools.com/api/films/6';
+const movieID = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieID;
 
-if (process.argv.length > 2) {
-  request(`${BASE_URL}/films/${process.argv[2]}/`, (err, res, body) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(JSON.parse(body).title);
-  });
-}
+request(url, (error, response, body) => {
+  if (error) { console.log(error); }
+  const jsonBody = JSON.parse(body);
+  console.log(jsonBody.title);
+});
